98786a135ec75274b383923022fbaf49f6a8257f
laurqsr <santosribeiro.laura@gmail.com>
2024-07-03 08:29:42 -0300

    aula20

--- aula19/Conta.java
+++ aula19/Conta.java
@@ -1,20 +1,37 @@
 package aula19;
 
 import java.util.Random;
-
 public class Conta {
+
+    float saldo;
     String titular;
     String identificador;
-    float saldo;
+    String senha;
 
-    void geraIdentificador(){
-        char letra = (char) new Random().nextInt(65,91);
-        int valor = new Random().nextInt(1000,10000);
-        identificador = letra+"_"+valor;
+    public Conta(String titular, String senha, float saldo){
+        geraIdentificador();
+        this.titular=titular;
+        this.saldo=saldo;
+        this.senha = senha;
     }
 
+    public Conta(String titular){
+        geraIdentificador();
+        this.titular = titular;
+    }
+    void geraIdentificador(){
+        //char letra = (char) new Random().nextInt(65,91);
+        //int valor = new Random().nextInt(1000, 10000);
+        //identificador= letra+"_"+valor;
+        String texto = "";
+        for(int i=0;i<4;i++){
+            texto+=(char) new Random().nextInt(65, 91);
+        }
+        int valor = new Random().nextInt(1000, 10000);
+        identificador = texto+valor;
+    }
     void depositar(float valor){
-        saldo+=valor;
+        this.saldo+=saldo; //this serve para referência à classe ou objeto onde se está.
     }
     boolean sacar(float valor){
         if(valor<=saldo){
@@ -23,7 +40,10 @@ public class Conta {
         }
         return false;
     }
+
     String verificaSaldo(){
-        return "Saldo atual R$ "+String.format("%.2f",saldo);
+        return "Saldo atual R$ "+String.format("%.2f", saldo);
     }
+
+
 }
